4391dc3be3fda2435d7ab942eaeb343d82698ac9
Erik <pipionedik@gmail.com>
2025-02-26 18:14:03 +0200

    First commit

---
+++ js/nav.js
@@ -0,0 +1,14 @@
+let a = 22;
+let b = 23;
+
+if (a >= b) {
+    console.log('Man really')
+} else {
+    console.log('GG BRO')
+}
+
+let margo = 5
+
+let putin = "zel" 
+let luda = "washington"
+console.log(margo + putin) 
